040ae7c3ab69164fc4b12f19ce4ae541502bd2bc
Salim <kelbiyev11@mail.ru>
2025-04-30 16:59:49 +0400

    complete?

---
+++ 02.26/regex.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <regex>
+
+int main() {
+    std::string inputLine;
+    
+    std::regex check1 {R"((?:(?: *(?:[\-]?\d+[\.]?\d*) *(?:[\-+*\^/]| *))*)\s*[\-]?\d+[\.]?\d*\s*)"};
+    std::regex check2 {R"(\s+)"};
+
+    std::cout << "Write your expression: ";
+    std::getline(std::cin, inputLine);
+    
+    if (std::regex_match(inputLine, check1))
+        std::cout << "Expression matches.\n";
+    else
+        std::cout << "Expression doesn't match.\n";
+        
+    std::string cleanedGap = std::regex_replace(inputLine, check2, "");
+    std::cout << "Expression without gaps: " << cleanedGap;
+    return 0;
+}
